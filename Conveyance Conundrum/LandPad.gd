7475eb8c91967f77ec6fc59f9af4e13aeb91e07b
Daniel Varela <dvd6412@gmail.com>
2017-10-30 23:36:12 -0400

    Misterio del Area2D resuelto
    faltaba la spooky collision shape

--- Conveyance Conundrum/LandPad.gd
+++ Conveyance Conundrum/LandPad.gd
@@ -6,10 +6,13 @@ extends Area2D
 
 func _ready():
 	# Called every time the node is added to the scene.
-
+	pass
 
 func _on_LandPad_body_exit( body ):
 	print("aaaaaaaaaaa")
 	if body.is_in_group("jumper"):
 		body.set_collision_mask_bit(1, true)
 		body.set_layer_mask_bit (1, true)
+
+func _on_LandPad_body_enter( body ):
+	print("nu")
