730ef5eab6b0e21d9853b88e98be09c92b95427f
Carlos Infante <carlosinfante1220@gmail.com>
2017-11-28 22:12:01 -0400

    Some state-ish stuff

--- Conveyance Conundrum/Movement Scrips/PatrolBot.gd
+++ Conveyance Conundrum/Movement Scrips/PatrolBot.gd
@@ -6,8 +6,11 @@ func _ready():
 	states.append(IdleState.new())
 	states.append(PursueState.new(get_parent(), player))
 	var playerClose = TargetClose.new(get_parent(), player, 300)
+	var playerHidden = TargetHidden.new(get_parent(), player)
 	playerClose.setTargetState(states[1])
+	playerHidden.setTargetState(states[0])
 	states[0].setTransitions([playerClose])
+	states[1].setTransitions([playerHidden])
 	initialState = states[0]
 	currentState = initialState
 	set_fixed_process(true)
--- Conveyance Conundrum/Movement Scrips/StateLib.gd
+++ Conveyance Conundrum/Movement Scrips/StateLib.gd
@@ -34,6 +34,32 @@ class PursueState:
 	func getEntryAction():
 		character.get_node("SamplePlayer2D").play("alarm")
 
+class TargetHidden:
+	extends "Transition.gd"
+	
+	var target
+	var radius
+	var character
+	
+	func _init(ch, tg):
+		self.character = ch
+		self.target = tg
+	
+	func isTriggered():
+		var space_state = character.get_world_2d().get_direct_space_state()
+		var result = space_state.intersect_ray(
+			character.get_pos(),
+			target.get_pos(),
+			[character, target]
+		)
+		return not result.empty()
+
+	func getAction():
+		var sprite = character.get_node("Sprite")
+		sprite.set_texture(load("res://bomba.png"))
+		character.get_node("SamplePlayer2D").stop_all()
+		character.steering = SteeringBehavior.new()
+
 class TargetClose:
 	extends "Transition.gd"
 	
@@ -55,7 +81,6 @@ class TargetClose:
 				target.get_pos(),
 				[character, target]
 			)
-			print(result.size())
 			return result.empty()
 		return false
 		
